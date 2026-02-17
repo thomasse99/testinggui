5f87bf7c35facb89e52b3f4539b1f94205443499
unk286 <177930405+unk286@users.noreply.github.com>
2026-02-17 08:21:23 +0000

    Update grids.py

--- grids.py
+++ grids.py
@@ -2,8 +2,22 @@ import tkinter as tk
 
 root = tk.Tk()
 
-tk.Button(text="Button").grid(row=1, column=1)
-tk.Button(text="Button").grid(row=1, column=2)
-tk.Button(text="Button").grid(row=2, column=1)
+root.geometry("+500+400")
+
+def get_name(event=None):
+    username = name.get()
+    if not username:
+        return None
+    for widget in root.winfo_children():
+        widget.destroy()
+    tk.Label(root, text=f"Hi there {username}", font=("font", 15)).pack(pady=10,padx=10)
+    
+
+tk.Label(root, text="Enter Name:").grid(row=0,column=1, pady=8, padx=8)
+name = tk.Entry(root)
+name.grid(row=0, column=2, pady=8, padx=8)
+name.bind("<Return>", get_name)
+enter_button = tk.Button(root, text="Enter",command=get_name)
+enter_button.grid(row=1,column=1,columnspan=2, pady=8, padx=8)
 
 root.mainloop()
